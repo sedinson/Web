4d4f194593fe9d15bfe126aec2da983467a0dbfe
Andrés <Andrs@andres-pc>
2012-05-14 16:50:30 -0500

    Parte inicial de Estimacion. Se añade el script para calcular el Inverso de la Normal Acumulada

---
+++ Controllers/EstimacionController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Description of EstimacionController
+ *
+ * @author Andrés A. Pérez L.
+ */
+
+class EstimacionController extends ControllerBase{
+    function Media(){
+        $this->view->partial("media.php");
+    }
+    function Varianza(){
+        
+    }
+    function Proporcion(){
+        
+    }
+    function DiferenciaDeMedia(){
+        
+    }
+    function CoeficienteVarianzas(){
+        
+    }
+    function DiferenciaMedias(){
+        
+    }
+}
+?>
---
+++ Views/media.php
@@ -0,0 +1,21 @@
+<script type="text/javascript" src="../Scripts/js/Estimacion.js"></script>
+<div>
+    <h1>Intervalos de Confianza para la Media: </h1>
+    <form>
+        <div>
+            <div>
+                <label for="alfa">&alpha;:</label>
+                <input id="alfa" type="number"/>
+            </div>
+            <div>
+                <label for="alfa">&micro;:</label>
+                <input id="alfa" type="number"/>
+            </div>
+            <div>
+                <label for="miu">&micro;:</label>
+                <input id="miu" type="number"/>
+            </div>
+
+        </div>
+    </form>
+</div>
