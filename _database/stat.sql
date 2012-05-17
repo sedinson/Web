ed5e381b756b593477f95b44bb241cb1de342937
sedinson <sedinson@Edinson.lan>
2012-05-16 23:08:34 -0500

    Se crearon todos los accesos directos a usar. Se alteró la base de datos. Por favor, si van a agregar algo trabajar con la última versión de esta, ubicada en _database/stat.sql.
    El archivo accesos.svg contiene la mayoria de las imágenes de los accesos vectorizados si se necesita para algo.

--- _database/stat.sql
+++ _database/stat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 17, 2012 at 04:28 AM
+-- Generation Time: May 17, 2012 at 06:05 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `access` (
   `image` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `url` text COLLATE utf8_spanish2_ci NOT NULL,
   PRIMARY KEY (`idaccess`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=31 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=37 ;
 
 --
 -- Dumping data for table `access`
@@ -41,19 +41,19 @@ CREATE TABLE IF NOT EXISTS `access` (
 
 INSERT INTO `access` (`idaccess`, `idparent`, `title`, `image`, `url`) VALUES
 (6, 0, 'Medidas', '73e079image3014.png', 'Diferentes tipos de medidas: de centralizaci&oacute;n, de variabilidad, de posici&oacute;n y de forma'),
-(7, 0, 'Graficas', '24e450g3862.png', 'Dibujar todo tipo de gr&aacute;ficas como torta, barra, etc.'),
+(7, 0, 'Gr&aacute;ficas', '24e450g3862.png', 'Dibujar todo tipo de gr&aacute;ficas como torta, barra, etc.'),
 (9, 0, 'Distribuciones', '9a5626g6080.png', 'Diferentes tipos de distribuciones de probabilidad'),
-(10, 7, 'Torta', '82a3e324e450g3862.png', 'Graficas/Torta'),
-(11, 7, 'Barra', '9c3079g5488.png', 'Graficas/Barra'),
+(10, 7, 'Gr&aacute;fica Circular', '82a3e324e450g3862.png', 'Graficas/Torta'),
+(11, 7, 'Diagrama de Barras', '9c3079g5488.png', 'Graficas/Barra'),
 (12, 7, 'Caja y Bigotes', 'dff5a1g5504.png', 'Graficas/CajayBigotes'),
-(13, 7, 'Frecuencia Acumulada', '7184e0g5777.png', 'Graficas/FrecuenciaAcumulada'),
+(13, 7, 'Diagrama de Frecuencia Acumulada', '7184e0g5777.png', 'Graficas/FrecuenciaAcumulada'),
 (14, 0, 'Estimaci&oacute;n', 'f7cdfeg4652.png', 'Estimaciones por intervalos e intervalos de confianza'),
-(15, 0, 'Sobre Nosotros', 'e8619ag5289.png', 'Descubre quienes trabajaron en esta aplicaci&oacute;n y como contribuir'),
+(15, 0, 'Sobre la Aplicaci&oacute;n', 'e8619ag5289.png', 'Descubre quienes trabajaron en esta aplicaci&oacute;n y como contribuir'),
 (16, 15, 'Quienes Colaboraron', 'a4d7f5g5289.png', 'About/Quienes'),
 (17, 15, 'Como Contribuir', '8b1bf2g5789.png', 'About/Como'),
 (18, 9, 'Normal', '6673b0g6080.png', 'Distribucion/Normal'),
-(19, 6, 'Centralizaci&oacute;n', 'b6730373e079image3014.png', 'Medidas/Centralizacion'),
-(20, 6, 'Variabilidad', 'c5145a73e079image3014.png', 'Medidas/Variabilidad'),
+(19, 6, 'Medidas de Centralizaci&oacute;n', 'b6730373e079image3014.png', 'Medidas/Centralizacion'),
+(20, 6, 'Medidas de Variabilidad', 'c5145a73e079image3014.png', 'Medidas/Variabilidad'),
 (21, 15, 'Documentaci&oacute;n', '06c7c8g6253.png', 'About/Documentacion'),
 (23, 14, 'Intervalo de Confianza para la Media', 'f18a70miu.png', 'Estimacion/media'),
 (24, 14, 'Intervalo de Confianza para la Varianza', '237b47sigma.png', 'Estimacion/Varianza'),
@@ -62,7 +62,13 @@ INSERT INTO `access` (`idaccess`, `idparent`, `title`, `image`, `url`) VALUES
 (27, 14, 'Intervalo para el Coeficiente de Varianzas', '94aaadcoefVar.png', 'Estimacion/CoeficienteVarianzas'),
 (28, 14, 'Intervalo para la Diferencia de Proporciones', 'ac35fddifP.png', 'Estimacion/DiferenciaProporcion'),
 (29, 14, 'Intervalo de PredicciÃ³n', 'a8b320prediccion.png', 'Estimacion/Prediccion'),
-(30, 14, 'Intervalo de Tolerancia', 'c3fe8ftolerancia.png', 'Estimacion/Tolerancia');
+(30, 14, 'Intervalo de Tolerancia', 'c3fe8ftolerancia.png', 'Estimacion/Tolerancia'),
+(31, 7, 'Diagrama de Frecuencia', 'e8e7c3g4647.png', 'Graficas/Frecuencia'),
+(32, 7, 'Diagrama de Puntos', 'c07571g4648.png', 'Graficas/Puntos'),
+(33, 7, 'Diagrama de Pareto', '2470e6g4649.png', 'Graficas/Pareto'),
+(34, 7, 'Diagrama de Ojiva', '2382c3g4650.png', 'Graficas/Ojiva'),
+(35, 6, 'Medidas de Posici&oacute;n', '07849bg4651.png', 'Medidas/Posicion'),
+(36, 7, 'Ingresar Datos Manualmente', '5701e806c7c8g6253.png', 'Graficas/Datos');
 
 -- --------------------------------------------------------
 
